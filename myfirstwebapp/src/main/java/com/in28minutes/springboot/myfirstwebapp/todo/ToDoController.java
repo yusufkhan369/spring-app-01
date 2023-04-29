7acab7236bbe98cd96a155d99427d498f322d8c2
coursera.zam <mdyusufk999@gmail.com>
2023-04-29 23:54:59 +0530

    Refactored code

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ToDoController {
-	
+
 	private ToDoService doService;
-	
+
 	public ToDoController(ToDoService doService) {
 		super();
 		this.doService = doService;
@@ -25,19 +25,18 @@ public class ToDoController {
 		model.addAttribute("todos", list);
 		return "listTodos";
 	}
-	
-	//GET, POST
-	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+
+	// GET, POST
+	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
 	public String showNewTodoPage() {
 		return "todo";
 	}
-	
-	//GET, POST
+
+	// GET, POST
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
 	public String addNewTodo(@RequestParam String description, ModelMap map) {
-		doService.addTodo((String)map.get("name"), description, LocalDate.now().plusDays(1), false);
+		doService.addTodo((String) map.get("name"), description, LocalDate.now().plusDays(1), false);
 		return "redirect:list-todos";
 	}
-	
-	
+
 }
--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoService.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoService.java
@@ -8,20 +8,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ToDoService {
-	
+
 	private static List<ToDo> toDos = new ArrayList<ToDo>();
 	private static int todosCount = 0;
-	
+
 	static {
 		toDos.add(new ToDo(++todosCount, "in28minutes", "desc", LocalDate.now().plusDays(1), false));
 		toDos.add(new ToDo(++todosCount, "zam2", "desc2", LocalDate.now().plusDays(2), false));
 		toDos.add(new ToDo(++todosCount, "zam3", "desc3", LocalDate.now().plusDays(3), false));
 	}
-	
-	public List<ToDo> findByUsername(String username){
+
+	public List<ToDo> findByUsername(String username) {
 		return toDos;
 	}
-	
+
 	public void addTodo(String username, String description, LocalDate date, boolean done) {
 		ToDo toDo = new ToDo(++todosCount, username, description, date, done);
 		toDos.add(toDo);
