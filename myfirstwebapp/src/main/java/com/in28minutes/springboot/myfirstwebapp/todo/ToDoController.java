f7789451336aaccf01e1b4a3e53f4b481dd92428
coursera.zam <yusuf47595@gmail.com>
2023-04-29 23:31:30 +0530

    final commit for the day

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/ToDoController.java
@@ -1,10 +1,12 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -23,5 +25,19 @@ public class ToDoController {
 		model.addAttribute("todos", list);
 		return "listTodos";
 	}
-
+	
+	//GET, POST
+	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	//GET, POST
+	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
+	public String addNewTodo(@RequestParam String description, ModelMap map) {
+		doService.addTodo((String)map.get("name"), description, LocalDate.now().plusDays(1), false);
+		return "redirect:list-todos";
+	}
+	
+	
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -1,7 +1,12 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
+	<head>
+		<link href="webjars/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" >
+		<title>List Todos Page</title>		
+	</head>
 	<body>
-		<div>
+		<div class="container">
 			<h1>Your Todos</h1>
 			<table class="table">
 				<thead>
@@ -23,8 +28,10 @@
 					</c:forEach>
 				</tbody>
 			</table>
-
+		<a href="add-todo" class="btn btn-success">Add Todo</a>
 		</div>
+		<script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+		<script src="webjars/jquery/3.6.0/jquery.min.js"></script>
 		
 	</body>
 </html>
